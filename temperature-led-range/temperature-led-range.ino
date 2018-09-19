c209aab58b0652525d00c1268306700140ed23c2
Ilja <iljapanic@gmail.com>
2018-09-19 19:02:28 +0200

    add led temperature with range

---
+++ temperature-led-range/temperature-led-range.ino
@@ -0,0 +1,47 @@
+int sensorPin = 0;
+int LedRed = 13;
+int Ledwhite = 12;
+
+float high = 33;
+float mid = 29;
+
+void setup() 
+{
+pinMode(LedRed, OUTPUT);
+pinMode(Ledwhite, OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int reading = analogRead(sensorPin); 
+float voltage = reading*5.0;
+voltage/= 1024.0;
+
+float temperatureC = (voltage-0.5)*100;
+Serial.print(temperatureC);
+Serial.println("degrees C");
+
+
+
+if (temperatureC > high) {
+  analogWrite(Ledwhite, 0);
+  analogWrite(LedRed, 255);
+  delay(10);
+}
+
+if (temperatureC > mid && temperatureC < high) {
+  analogWrite(Ledwhite, 255);
+  analogWrite(LedRed, 255);
+  delay(10);
+}
+
+if (temperatureC < mid) {
+  analogWrite(Ledwhite, 255);
+  analogWrite(LedRed, 0);
+  delay(10);
+}
+
+delay(500);
+}
