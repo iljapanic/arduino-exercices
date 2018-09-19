ff682755d41e05598454381cf4446d5d92bed2af
Ilja <iljapanic@gmail.com>
2018-09-19 11:21:40 +0200

    add existing excercises

---
+++ hippie-police/hippie-police.ino
@@ -0,0 +1,121 @@
+// asign inputs-outputs to pins - stay constant
+int ledBlue = 8;
+int ledRed = 9;
+int ledGreen = 7;
+int ledYellow = 6;
+int button = 10;
+
+// variables that will change throughout
+int buttonPushCounter = 0;
+int buttonState = 0;
+int lastButtonState = 0;
+
+
+void setup() {
+  pinMode(ledBlue, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+}
+
+
+void turnLightsOn() {
+  digitalWrite(ledRed, HIGH);
+  digitalWrite(ledBlue, HIGH);
+  digitalWrite(ledGreen, HIGH);
+  digitalWrite(ledYellow, HIGH);
+}
+
+void turnLightsOff() {
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledBlue, LOW);
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledYellow, LOW);
+}
+
+
+void loop() {
+
+  buttonState = digitalRead(button);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
+    } else {
+      // if the current state is LOW then the button went from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
+
+
+  // turns on the LED every four button pushes by checking the modulo of the
+  // button push counter. the modulo function gives you the remainder of the
+  // division of two numbers:
+  if (buttonPushCounter % 2 == 0) {
+    turnLightsOn();
+  } else {
+    turnLightsOff();
+  }
+
+  
+}
+
+
+
+
+
+
+  
+//
+//  if (buttonState == HIGH) {
+//    digitalWrite(ledRed, HIGH);
+//    digitalWrite(ledBlue, HIGH);
+//    digitalWrite(ledGreen, HIGH);
+//    digitalWrite(ledYellow, HIGH);
+//  } else {
+//    digitalWrite(ledRed, LOW);
+//    digitalWrite(ledBlue, LOW);
+//    digitalWrite(ledGreen, LOW);
+//    digitalWrite(ledYellow, LOW);
+//  }
+
+
+//  digitalWrite(ledBlue, HIGH);
+//  digitalWrite(ledRed, HIGH);
+//  digitalWrite(ledGreen, HIGH);
+//  digitalWrite(ledYellow, HIGH);
+//  
+//  delay(200);
+//
+//  digitalWrite(ledBlue, HIGH);
+//  digitalWrite(ledRed, LOW);
+//  digitalWrite(ledGreen, LOW);
+//  digitalWrite(ledYellow, LOW);
+//
+//  delay(200);
+//
+//  digitalWrite(ledBlue, LOW);
+//  digitalWrite(ledRed, LOW);
+//  digitalWrite(ledGreen, HIGH);
+//  digitalWrite(ledYellow, LOW);
+//
+//  delay(200);
+//
+//  digitalWrite(ledBlue, LOW);
+//  digitalWrite(ledRed, LOW);
+//  digitalWrite(ledGreen, LOW);
+//  digitalWrite(ledYellow, HIGH);
+//
+//  delay(200);
---
+++ led-potential/led-potential.ino
@@ -0,0 +1,45 @@
+// asign inputs-outputs to pins - stay constant
+const int ledBlue = 8;
+const int ledRed = 9;
+const int ledGreen = 7;
+const int ledYellow = 6;
+const int meter = A0;
+
+// variables that will change throughout the loop
+int sensorValue = 0;
+int outputValue = 0;
+
+// initial setup
+void setup() {
+  pinMode(ledBlue, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(meter, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // read the potenciometer value
+  sensorValue = analogRead(meter);
+
+  // get potentiometer range (0-1023) and remap its to values for LEDs (0-255)
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+
+  // pass remaped values to LEDs
+  analogWrite(ledRed, outputValue);
+  analogWrite(ledBlue, outputValue);
+  analogWrite(ledGreen, outputValue);
+  analogWrite(ledYellow, outputValue);
+
+  // print the values to Serial for easier debugging
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print(" ~ ");
+  Serial.print("output = ");
+  Serial.println(outputValue);
+  
+  delay(10);
+    
+}
