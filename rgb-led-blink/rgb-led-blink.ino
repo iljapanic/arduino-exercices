8ff05a7517748403a8ca817ed753292bbd5f6d12
Ilja <iljapanic@gmail.com>
2018-09-20 10:34:57 +0200

    add rgb blinking led

---
+++ rgb-led-blink/rgb-led-blink.ino
@@ -0,0 +1,38 @@
+// store pin numbers into variables
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+
+
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  blink();
+
+}
+
+void blink() {
+  // red
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  delay(1000);
+
+  // green
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, LOW);
+  delay(1000);
+
+  // blue
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, HIGH);
+  delay(1000);
+}
